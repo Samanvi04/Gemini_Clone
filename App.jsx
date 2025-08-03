e3da1b4b155b9b3baf3615ab115effda797b0ed8
Samanvi <4samanvi@gmail.com>
2025-08-03 18:21:20 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from "./Components/Main/main"
+import Sidebar from "./Components/Sidebar/Sidebar"
+
+const App = () => {
+    return (
+        <>
+            <Sidebar/>
+            <Main/>
+        </>
+    )
+}
+
+export default App;
